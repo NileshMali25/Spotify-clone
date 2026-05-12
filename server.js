4ed8013c9054504247c493025b51f314a5a0bf60
NileshMali25 <nilu12969@gmail.com>
2026-05-13 04:08:55 +0530

    Fix Express 5 wildcard route bug

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const express = require('express');
 
 app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
